25d01cbd0c24e06077204b8d6d5f5dd0850209e4
NicolasEyzaguirre <nicolas.eyzaguirre12@gmail.com>
2022-12-13 07:08:52 +0100

    cambio instruc.

--- hunde_la_flota.py
+++ hunde_la_flota.py
@@ -3,7 +3,7 @@ from fun_hlf import *
 
 print("bienvenido al juego de battleship")
 nombre_jugador=input("por favor introduzcas su nombre de jugador: ")
-print("los barcos se pondran de manera aleatoria")
+print("sus barcos estan seleccionados de manera predeterminada")
 
 print(barcos_jugador(([(0,1), (1,1)] , [(0,9), (1,9)] , [(9,1), (9,2)] , [(4,3), (4,4), (4,5), (4,6)]  , [(1,3), (1,4), (1,5)] , [(9,4), (9,5), (9,6)] , [(5,1)] , [(4,6)] ,[(5,8)] , [(7,7)]))) 
 tablero_maquina_2=np.full((10,10),' ')
